2eef5727cf2e8f2ab5679f9bd6f8e061fa4f23b9
ghxsty-dev <GitHubEpostaAdresin>
2026-07-17 01:20:48 +0300

    Loading screen tamamen kaldirildi

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,5 @@
-import LoadingScreen from "@/components/LoadingScreen"
 import "@/styles/globals.css"
 
 export default function App({ Component, pageProps }) {
-  return (
-    <LoadingScreen>
-      <Component {...pageProps} />
-    </LoadingScreen>
-  )
+  return <Component {...pageProps} />
 }
